c1b7f3e21c62c8bec6c5d245e94525db51892600
Dendrobyte <markbacon78@gmail.com>
2022-05-21 15:57:21 -0400

    Swear filter turned into helper function

--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,8 @@ module.exports = {
         console.log(`Oinkbot, disguised as ${client.user.tag}, is ready!`);
 
         // Initalize youtubevideo notifier
-        //const ytNotifier = require('../feats/ytnotifs/ytnotifclient')
-        //ytNotifier.start(client)
+        const ytNotifier = require('../feats/ytnotifs/ytnotifclient')
+        ytNotifier.start(client)
         // TODO: Disable this if the key is "0000" or something
     } 
 }
---
+++ feats/swearfilter/swearFilterHelper.js
@@ -0,0 +1,53 @@
+const swears = require('./swears.json')
+const config = require('../../config.json')
+const Discord = require('discord.js');
+
+let filterMessage = async (message, author,client) => { 
+    let content = message.cleanContent.toLowerCase();
+    let simple = true
+
+    // See if a simple swear exists
+    let foundWord = await content.split(/\s+/).find(word => swears.simple[word] !== undefined)
+    if (foundWord === undefined) {
+        // Check for more "complex" curses via regex matching
+        foundWord = await content.split(/\s+/).find(word => {
+            Object.keys(swears.complex).forEach(complexSwear => {
+                if (word.match(new RegExp(complexSwear))){
+                    simple = false
+                    return word
+                }
+            })
+        })
+    }
+
+    // Thus, check to see if we have found a swear word present in the message
+    if (foundWord !== undefined){
+        await message.reply('Swearing is not allowed :eyes:')
+        let wordCategory = simple === true ? swears['simple'][foundWord].join(' and ') : swears['complex'][foundWord].join(' and ')
+        const response = new Discord.MessageEmbed()
+            .setAuthor({ name: 'Please refrain from swearing!' })
+            .addField("You were caught on", foundWord)
+            .addField("It was categorized as:", wordCategory)
+            .setColor('#ff0000')
+            .setFooter({ text: 'This message was also logged to the staff team.'});
+        author.createDM().then(channel => channel.send(
+            {embeds: [response]}
+        ))
+        message.delete({wait: 0})
+
+        // Log to staff channel
+        const log = new Discord.MessageEmbed()
+            .setAuthor({ name: "Swear blocked." })
+            .addField("User responsible: ", author.username)
+            .addField("Caught word:", foundWord)
+            .addField("Categorized as:", wordCategory)
+            .setColor("#FFA500")
+        client.channels.cache.get(config.channels.auditLog).send(
+            {embeds: [log]}
+        )
+        return true;
+    }
+    return false;
+}
+
+module.exports.filterMessage = filterMessage
